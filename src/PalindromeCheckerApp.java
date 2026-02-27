9234a55ea674912fcdaabead8b8e1d9ce8376558
Alisha1904 <alishaswain15@gmail.com>
2026-02-27 10:56:08 +0530

    use case one done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,23 @@
 public class PalindromeCheckerApp {
+    static final String APP_VERSION = "1.0.0";
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("=======================================");
+        System.out.println("     Welcome to Palindrome Checker App ");
+        System.out.println("=======================================");
+
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : " + APP_VERSION);
+        System.out.println("---------------------------------------");
+
+
+        System.out.println("System is ready for palindrome validation...");
+        System.out.println("Thank you for using the application.");
+        System.out.println("-----------------------");
+    }
+
 }
