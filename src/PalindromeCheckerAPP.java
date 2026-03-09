1b8ecf1fd6a45c0942674ed1bc196f3da071d000
Alisha1904 <alishaswain15@gmail.com>
2026-03-09 10:05:03 +0530

    UC9

--- src/PalindromeCheckerAPP.java
+++ src/PalindromeCheckerAPP.java
@@ -1,82 +1,27 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-public class PalindromeCheckerAPP {
+public class PalindromeCheckerAPP{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input: ");
+        System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
-            System.out.println("Is Palindrome? : true");
-        } else {
-            System.out.println("Is Palindrome? : false");
-        }
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+        System.out.println("Is Palindrome? : " + isPalindrome);
 
         scanner.close();
     }
 
-    public static boolean isPalindrome(String str) {
-        if (str == null || str.isEmpty())
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
             return true;
-
-        // Create linked list from string
-        Node head = new Node(str.charAt(0));
-        Node current = head;
-
-        for (int i = 1; i < str.length(); i++) {
-            current.next = new Node(str.charAt(i));
-            current = current.next;
         }
 
-        // Find middle using slow & fast pointer
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
 
-        // Reverse second half
-        Node secondHalfHead = reverseList(slow);
-        Node firstHalfHead = head;
-
-        // Compare both halves
-        Node tempSecond = secondHalfHead;
-        while (tempSecond != null) {
-            if (firstHalfHead.data != tempSecond.data) {
-                return false;
-            }
-            firstHalfHead = firstHalfHead.next;
-            tempSecond = tempSecond.next;
-        }
-
-        return true;
-    }
-
-    // Reverse linked list
-    private static Node reverseList(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        return prev;
+        return check(s, start + 1, end - 1);
     }
 }
